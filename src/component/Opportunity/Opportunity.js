b80d8ac551983cfab53428e8dcc2dd43fbf7ace1
Obaid <obaid3035@gmail.com>
2023-03-01 01:19:35 +0500

    Production build 10

--- src/component/Opportunity/Opportunity.js
+++ src/component/Opportunity/Opportunity.js
@@ -5,7 +5,7 @@ import {Container, Row} from "react-bootstrap";
 const Opportunity = () => {
 	return (
 		<Container className={styles.Opportunity}>
-			<Row className={'justify-content-center'}>
+			<Row className={'justify-content-start'}>
 				<h1>Investment Opportunities</h1>
 				<p className={'mt-4'}>Surrounded by unspoiled <span>coves</span>, cerulean <span>lagoons</span>, white-sandy <span>beaches</span> and lush green <span>pastures</span>,
 					DAMAC Lagoons is a <span>perfect investment</span> whether as a private residence or with the purpose of generating a <span>passive income</span>.
