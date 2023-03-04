9950cdb201a47d626e4e4c91f8a95ddc08276fca
Obaid <obaid3035@gmail.com>
2023-03-05 01:58:20 +0500

    Production build 19

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,23 @@
 import '@/styles/globals.css'
+import Script from 'next/script'
 
 //Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}) {
+
+	return (
+		<>
+			<Script strategy={'afterInteractive'} src={'https://www.googletagmanager.com/gtag/js?id=G-G3Z7777F74'}/>
+			<Script id={'google-analytics'} strategy={'afterInteractive'}>
+				{`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-G3Z7777F74');
+            `}
+			</Script>
+			<Component {...pageProps} />
+		</>
+	)
 }
