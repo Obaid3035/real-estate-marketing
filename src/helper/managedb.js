a554a3791625b7e665327013bb31c4d7c7eb6832
Obaid <obaid3035@gmail.com>
2023-03-16 21:58:27 +0500

    Production build 23

--- src/helper/managedb.js
+++ src/helper/managedb.js
@@ -3,20 +3,6 @@ import {GoogleSpreadsheet} from "google-spreadsheet";
 export async function writeInfo(Name, Phone, Email) {
 	try {
 		const doc = new GoogleSpreadsheet(process.env.SHEET_ID)
-		console.log({
-			type: process.env.TYPE,
-			project_id: process.env.PROJECT_ID,
-			private_key_id: process.env.PRIVATE_KEY_ID,
-			private_key: process.env.PRIVATE_KEY,
-			client_email: process.env.CLIENT_EMAIL,
-			client_id: process.env.CLIENT_ID,
-			auth_uri: process.env.AUTH_URI,
-			token_uri: process.env.TOKEN_URI,
-			auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
-			client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
-			SHEET_ID: process.env.SHEET_ID
-
-		})
 		await doc.useServiceAccountAuth({
 			type: process.env.TYPE,
 			project_id: process.env.PROJECT_ID,
